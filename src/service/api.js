01aceeb5adb218b99ca92aa9ba78d92827483da8
Italo Alves <contato.italo1020@gmail.com>
2021-01-31 18:42:42 -0300

    add api link

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../redux/store";
 
 const local = "http://127.0.0.1:3333";
-const remote = "http://54.82.68.184:3333";
+const remote = "https://www.belopao.com";
 
 const api = axios.create({
   baseURL: remote,
